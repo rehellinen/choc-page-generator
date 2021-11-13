0a4182efb40d20ed830cc63e045414d625d3a6e6
rehellinen <rehellinen@foxmail.com>
2021-11-13 15:28:56 +0800

    feat: init project with yarn workspace

---
+++ README.md
@@ -0,0 +1,6 @@
+# Chocolate Page Generator
+### 定位
+1. 通过一个配置项来生成管理台页面
+2. 适用于React + Ant Design技术栈
+
+
--- src/components/Form/utils.ts
+++
@@ -1,5 +0,0 @@
-import { FormSchema } from '../../types/Form';
-
-export function getAllProps<T extends object>(config: FormSchema<T>[]): string[] {
-  return config.map((item) => item.name as string);
-}
