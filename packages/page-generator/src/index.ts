96825756cb055d7c7c0d93f3c516dabd0cb2e409
rehellinen <rehellinen@foxmail.com>
2022-07-31 16:37:31 +0800

    refactor: delete useless code and files

--- packages/page-generator/src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './PageContainer'
-export * from './utils/constant'
-export { registerComponent } from './components'
--- packages/page-generator/src/utils/utils.ts
+++ packages/page-generator/src/utils/utils.ts
@@ -1,40 +1,16 @@
+import { message } from 'antd'
+
+export function isDef<T>(val: T): val is NonNullable<T> {
+  return val !== null
+}
+export function isPromise(val: any): val is Promise<any> {
+  return isDef(val) && typeof val.then === 'function' && typeof val.catch === 'function'
+}
 export const isObject = (val: any) => val !== null && typeof val === 'object'
 export const isString = (val: any) => typeof val === 'string'
 export const isFunction = (val: any) => typeof val === 'function'
 
-export const parseText = (text: string) => {
-  const tagRE = /\{\{((?:.|\r?\n)+?)\}\}/g
-  if (!tagRE.test(text)) {
-    return {
-      isStatic: true,
-      exp: JSON.stringify(text)
-    }
-  }
-
-  const tokens: any[] = []
-  let index
-  let lastIndex = 0
-  tagRE.lastIndex = 0
-
-  let match = tagRE.exec(text)
-  while (match) {
-    const [matchedStr, exp] = match
-    index = match.index
-
-    if (index > lastIndex) {
-      tokens.push(JSON.stringify(text.slice(lastIndex, index)))
-    }
-    tokens.push(`(${exp.trim()})`)
-    lastIndex = index + matchedStr.length
-
-    match = tagRE.exec(text)
-  }
-  if (lastIndex < text.length) {
-    tokens.push(JSON.stringify(text.slice(lastIndex)))
-  }
-
-  return {
-    isStatic: false,
-    exp: tokens.join('+')
-  }
+export const handleError = (e: Error) => {
+  console.log(e)
+  message.error(e.message)
 }
